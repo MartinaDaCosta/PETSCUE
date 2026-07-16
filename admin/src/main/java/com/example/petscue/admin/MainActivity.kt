cd80b5c53892358d92c296b79537fbc1057795c7
mardaco2 <mardaco2@alumni.uv.es>
2026-07-16 15:13:03 +0200

    splash

--- admin/src/main/java/com/example/petscue/admin/MainActivity.kt
+++ admin/src/main/java/com/example/petscue/admin/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.petscue.admin.ui.navigation.AdminNavHost
 import com.example.petscue.admin.ui.theme.PETSCUETheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
